0c746c821ab38e97cffba781501fcb48b1947611
avalldeperas <albertvalldeperas@gmail.com>
2019-02-11 18:43:58 +0100

    eliminada branch nuevaBranch

--- src/championship/Championship.java
+++ src/championship/Championship.java
@@ -25,7 +25,7 @@ public class Championship {
         try {
             manager = new Manager();
             manager.init();
-            int option = -1;
+            int option = -333;
             do {
                 showMenu();
                 option = InputAsker.askInt("Choose an option");
